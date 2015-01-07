703165e3571c9e31fe8958c56fc9defa89a022ff
Rodrigo <reyesrr@gmail.com>
2015-01-07 20:27:37 +0100

    cleans up the main script

--- index.js
+++ index.js
@@ -6,48 +6,54 @@ var dockerlib = require("dockerlib"),
     fs =require("fs"),
     misc = require("./miscutils");
 
-var configLoader = new cfg.Loader();
 
-var cmdline_options = optimist
-    .boolean("verbose").default("verbose", false).alias("v", "verbose")
-    .usage("Usage: $0 config-file")
+var cmdLineParser = optimist
+        .option("v", { alias: "verbose", default: false, describe: "Verbose mode" }).boolean("v")
+        .boolean("extra-verbose").option("extra-verbose", {describe:"Extra verbose mode (really!)", default: false})
+        .option("w", { alias: "warns", default: false, describe: "Treat warnings as errors"}).boolean("w")
+        .option("s", { alias: "stacktrace", default: false, describe: "Eventually display the stacktrace"}).boolean("s")
+        .check(function(argv) {
+            if (argv._.length != 1) {
+                throw new Error("Expecting one argument");
+            }
+        })
+        .check(function(argv) {
+            if (!fs.existsSync(argv._[0])) {
+                throw new Error("Configuration file '"+argv._[0]+"' does not exist");
+            }
+        })
+        .usage("Usage: $0 [options] config-file")
     ;
+var options = cmdLineParser.argv
 
-var options = cmdline_options.argv
+process.on('uncaughtException', function (err) {
+    console.error((typeof err.message == "string" && err.message.substr(0,5).toLowerCase()!="error"?"Error: ":"") + (err.message?err.message:"something unexpected happend"));
+    if (options.stacktrace) {
+        console.error(err.stack);
+    }
+    console.log("Installation failed.");
+    process.exit(99);
+});
 
-if (options._.length != 1) {
-    console.error("Error: Needs one and only one configuration file");
-    process.exit(1);
-}
-var configurationFile = misc.interpolatePath(options._[0]);
-if (!fs.existsSync(configurationFile)) {
-    console.error("Error: Configuration file does not exist!");
-    process.exit(2);
-}
+var configurationFile = options._[0];
 
 var defaultLocalFile = "$HOME/.dockenv.ini";
-if (options.verbose && !fs.existsSync(defaultLocalFile)) {
-    console.log("Warning: local file " + defaultLocalFile + " not found");
+if (options.verbose && fs.existsSync(defaultLocalFile)) {
+    console.log("Using local configuration file '" + defaultLocalFile + "' as basis");
 }
+
+var configLoader = new cfg.Loader();
 configLoader.load(defaultLocalFile);
 var config = configLoader.load(configurationFile);
-
-if (config["registry-user"] && config["registry-domain"]) {
-    options.verbose && console.log("Login to " + config["registry-domain"] + " (user: " + config["registry-user"] + ")");
-    try {
-        dockerlib.docker.login(config["registry-user"], config["registry-domain"]);
-    } catch (e) {
-        console.error("Error: " + e.message);
-        console.error("You need to be logged in to the registry " + config["registry-domain"]);
-        process.exit(5);
-    }
+if (config.label) {
+    console.log("Environment: " + config.label);
 }
-
 var manager = new dockermgr.DockerEnvironmentManager(config, options.verbose);
-dockerlib.verbose = options.extraVerbose;
+dockerlib.verbose = options["extra-verbose"]
+manager.logIn();
 
 var checkResults = manager.verify();
-if (options.verbose && checkResults.warnings.length>0) {
+if ((options.verbose || options.warns) && checkResults.warnings.length>0) {
     checkResults.getWarningMessages().forEach(function(msg) {
         console.error("Warning: " + msg);
     });
@@ -56,19 +62,11 @@ if (checkResults.errors.length>0) {
     checkResults.getErrorMessages().forEach(function(msg) {
         console.error("Error: " + msg);
     });
-    process.exit(3);
+}
+if (checkResults.errors.length>0 || (options.warns && checkResults.warnings.length>0)) {
+    throw new Error("Errors were found in the configuration file.");
 }
 
-try {
-    manager.install();
-    console.log("Successfully installed " + configurationFile);
-    process.exit(0);
-} catch (e) {
-    if (options.verbose) {
-        throw e;
-    } else {
-        console.error(e.message);
-    }
-    console.log("Installation failed.");
-    process.exit(99);
-}
+manager.install();
+console.log("Successfully installed " + configurationFile);
+process.exit(0);
